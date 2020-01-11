e8ade517ac489c3b916bead2267a6ece46a5d7a2
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-11 11:20:05 +0900

    firebase を初期化して addMessage 関数を追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text;
+  const snapshot = await admin
+    .database()
+    .ref('/messages')
+    .push({ original: original });
+  res.redirect(303, snapshot.ref.toString());
+});
